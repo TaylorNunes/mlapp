eed7d5182ad31cb8d7d99c96e3980e3b16ce2924
Taylor <taylorcnunes@gmail.com>
2022-03-23 09:23:52 -0400

    Commented out audioclassification urls

--- myapp/mlapp/urls.py
+++ myapp/mlapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-  path('audioclassification/', include('audioclassification.urls')),    
+  #path('audioclassification/', include('audioclassification.urls')),    
   path('textclassification/', include('textclassification.urls')),    
   path('admin/', admin.site.urls),
 ]
